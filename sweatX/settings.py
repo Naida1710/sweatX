cf9837e6f67597091fff3f6167a4d833abc83868
Naida Hadzic <naida.popovic1993@gmail.com>
2025-12-21 20:20:14 +0100

    Add checkout views and templates

--- sweatX/settings.py
+++ sweatX/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
 
+    # Other
+    'crispy_forms',
+
 ]
 
 MIDDLEWARE = [
@@ -49,6 +52,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'sweatX.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -66,6 +71,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
+            ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
             ],
         },
     },
