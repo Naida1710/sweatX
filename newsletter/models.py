5d33c03b5938ea05144e60013a4b5f24fe9544fe
Naida Hadzic <naida.popovic1993@gmail.com>
2026-03-10 17:50:44 +0100

    Add newsletter app, and models.py

---
+++ newsletter/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class NewsletterSubscriber(models.Model):
+    email = models.EmailField(unique=True)
+    subscribed_at = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['-subscribed_at']
+
+    def __str__(self):
+        return self.email
