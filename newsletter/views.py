14bcbdf64e9a3d1a085692acf94270c7b3111b2e
Naida Hadzic <naida.popovic1993@gmail.com>
2026-03-10 17:52:23 +0100

    Create newsletter forms.py, views.py and urls.py

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect
+from .forms import NewsletterForm
+from .models import NewsletterSubscriber
 
-# Create your views here.
+
+def subscribe_newsletter(request):
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+
+        if form.is_valid():
+            email = form.cleaned_data['email'].lower().strip()
+
+            if NewsletterSubscriber.objects.filter(email=email).exists():
+                messages.warning(request, 'This email is already subscribed.')
+            else:
+                NewsletterSubscriber.objects.create(email=email)
+                messages.success(request, 'You have successfully subscribed to the sweatX newsletter!')
+
+        else:
+            messages.error(request, 'Please enter a valid email address.')
+
+    return redirect(request.META.get('HTTP_REFERER', 'home'))
