46f300a8a98bc081495e2b0eb0036339f24660b2
Naida Hadzic <naida.popovic1993@gmail.com>
2026-03-16 10:24:54 +0100

    Add robots.txt file to sweatX/templates folder

--- sweatX/urls.py
+++ sweatX/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,4 +15,8 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('newsletter/', include('newsletter.urls')),
     path('reviews/', include('reviews.urls')),
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
