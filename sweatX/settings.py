9ea3ae91128dd25457fb3d0ab0418100e86a36d0
Naida Hadzic <naida.popovic1993@gmail.com>
2026-05-14 12:44:25 +0200

    add logging config so production errors stream to stdout

--- sweatX/settings.py
+++ sweatX/settings.py
@@ -216,3 +216,26 @@ SITE_URL = os.environ.get('SITE_URL', 'http://127.0.0.1:8000')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
+
